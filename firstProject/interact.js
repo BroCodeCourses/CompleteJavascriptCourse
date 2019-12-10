c640dfff3cabcfb538a66b0c0f3b34f61304e59b
Nick Ecton <nickecton@Nicks-MacBook-Pro.local>
2019-12-10 00:56:17 -0800

    text edits

--- firstProject/interact.js
+++ firstProject/interact.js
@@ -2,12 +2,12 @@ let btn = document.querySelector('button')
 
 
 btn.addEventListener('click', () => {
-    let pStyle = document.querySelector('p').style.color;
-    if(pStyle === 'pink') {
-        document.querySelector('p').style.color = 'green';
-        document.querySelector('p').innerText = "now it's in green"
+    let pElement = document.querySelector('p');
+    if(pElement.style.color === 'pink' || !pElement.style.color) {
+        pElement.style.color = 'green';
+        pElement.innerText = "now it's in green"
     } else {
-        document.querySelector('p').style.color = 'pink';
-        document.querySelector('p').innerText = 'This first paragraph is in pink';
+        pElement.style.color = 'pink';
+        pElement.innerText = 'This first paragraph is in pink';
     }
 })
